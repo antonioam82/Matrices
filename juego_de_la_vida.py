d5877443fe9254a47462822b48b08abe3f747d6b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-01-31 19:36:05 +0100

    Update juego_de_la_vida.py

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -1,9 +1,17 @@
 print("JUEGO DE LA VIDA")
 import time
+from VALID import OKI
 import subprocess
-filas=10
-columnas=10
-
+filas=OKI(input("Introduce el número de filas: "))
+columnas=OKI(input("Introduce el número de columnas: "))
+pul=OKI(input("Introduce el número de pulsos: "))
+print("¿Que desea ver?: ")
+print("A) OSCILADOR")
+print("B) PLANEADOR")
+ver=input("Escriba aquí su opción: ")
+while ver!=("A") and ver!=("B"):
+    ver=input("Escriba \'A\' o \'B\' según su opción: ")
+subprocess.call(["cmd.exe","/C","cls"])
 
 tablero=[]
 el=0
@@ -13,11 +21,18 @@ for i in range(filas):
     #print(tablero[el])
     #el+=1
 
-tablero[4][5]=True
-tablero[5][5]=True
-tablero[6][5]=True
-
-
+if ver==("A"):
+    tablero[4][5]=True
+    tablero[5][5]=True
+    tablero[6][5]=True
+else:
+    tablero[7][7]=True
+    tablero[8][8]=True
+    tablero[8][9]=True
+    tablero[7][9]=True
+    tablero[6][9]=True
+print("JUEGO DE LA VIDA")
+print("TIME:  0")
 for y in range(filas):
     for x in range(columnas):
         if tablero[y][x]:
@@ -25,10 +40,10 @@ for y in range(filas):
         else:
             print("."+" ",end="")
     print()
-time.sleep(1)
+time.sleep(2)
 subprocess.call(["cmd.exe","/C","cls"])
 
-pulsos=10
+pulsos=pul
 for t in range(pulsos):
     print("JUEGO DE LA VIDA")
     nuevo=[]
@@ -46,13 +61,13 @@ for t in range(pulsos):
                 n+=1
             if y>0 and tablero[y-1][x]:
                 n+=1
-            if y<filas-1 and x>0 and tablero[y+1][x]:
+            if y<filas-1 and tablero[y+1][x]:
                 n+=1
             if y>0 and x<columnas-1 and tablero[y-1][x+1]:
                 n+=1
             if x<columnas-1 and tablero[y][x+1]:
                 n+=1
-            if y<filas-1 and x<columnas-1 and tablero[x+1][y+1]:
+            if y<filas-1 and x<columnas-1 and tablero[y+1][x+1]:
                 n+=1
                 
             if tablero[y][x] and (n==2 or n==3):
@@ -64,6 +79,7 @@ for t in range(pulsos):
 
     tablero=nuevo
     t+1
+    print("TIME: ",t+1)
     for y in range(filas):
         for x in range (columnas):
             if tablero[y][x]:
@@ -71,8 +87,18 @@ for t in range(pulsos):
             else:
                 print("."+" ",end="")
         print()
-    time.sleep(1)
+    time.sleep(2)
     subprocess.call(["cmd.exe","/C","cls"])
+
+
+    
+
+
+
+
+
+
+
 
 
     
