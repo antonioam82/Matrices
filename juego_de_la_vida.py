639826c2795bdc4fd7fe75206f8b591565628c53
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-11-29 19:53:28 +0100

    Update juego_de_la_vida.py

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -1,4 +1,3 @@
-#PROYECTO EN CURSO...
 import time
 import subprocess
 from VALID import ns, OK, OKI, opt
@@ -12,11 +11,12 @@ while True:
     print("A) OSCILADOR")
     print("B) PLANEADOR")
     print("C) COLISION PLANEADORES")
-    ver=opt(input("Escriba aquí su opción: "),["A","B","C"])
+    print("D) DISPARADOR DE PLANEADORES")
+    ver=opt(input("Escriba aquí su opción: "),["A","B","C","D"])
     vel=OK(input("Introduce segundos entre cada pulso: "))
     panel=(".")
-    filas=22
-    colum=22
+    filas=38
+    colum=75
     
     pul=nval(OKI(input("Introduce número de pulsos: ")))
 
@@ -51,6 +51,44 @@ while True:
         tablero[19][18]=True
         tablero[20][18]=True
         tablero[21][18]=True
+    if ver=="D":
+        tablero[4][1]=True
+        tablero[5][1]=True
+        tablero[4][2]=True
+        tablero[5][2]=True
+        tablero[4][11]=True
+        tablero[5][11]=True
+        tablero[6][11]=True
+        tablero[3][12]=True
+        tablero[7][12]=True
+        tablero[2][13]=True
+        tablero[8][13]=True
+        tablero[2][14]=True
+        tablero[8][14]=True
+        tablero[5][15]=True
+        tablero[3][16]=True
+        tablero[7][16]=True
+        tablero[4][17]=True
+        tablero[5][17]=True
+        tablero[6][17]=True
+        tablero[5][18]=True
+        tablero[2][21]=True
+        tablero[3][21]=True
+        tablero[4][21]=True
+        tablero[2][22]=True
+        tablero[3][22]=True
+        tablero[4][22]=True
+        tablero[1][23]=True
+        tablero[5][23]=True
+        tablero[0][25]=True
+        tablero[1][25]=True
+        tablero[5][25]=True
+        tablero[6][25]=True
+        tablero[2][35]=True
+        tablero[3][35]=True
+        tablero[2][36]=True
+        tablero[3][36]=True
+        
 
     print("JUEGO DE LA VIDA")
     print("PULSO: 0")
@@ -114,4 +152,7 @@ while True:
     conti=ns(input("¿Desea continuar?: "))
     if conti==("n"):
         break
-    subprocess.call(["cmd.exe","/C","cls"])
+    try:
+        subprocess.call(["cmd.exe","/C","cls"])
+    except:
+        continue
